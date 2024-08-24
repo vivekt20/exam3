830feab3d1c9f628beaf67e99894f6bad803f00c
vivekt20 <vivekcherupoika20@gmail.com>
2024-08-24 09:06:45 +0530

    pro1.dart

--- pro1.dart
+++ pro1.dart
@@ -3,7 +3,7 @@ void main(){
 stdout.write("enter the number:");
 var num1=int.parse(stdin.readLineSync()!);
 print("digits in reverse order: \n");
-if(num1>0){
+while(num1>0){
 int? digit=num1%10;
 print(digit);
 num1=num1~/10;
